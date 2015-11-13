0df0e5833c8f8857ff989d0c6a3305b643a8fa41
Dan McClain <git@danmcclain.net>
2015-11-12 19:36:27 -0500

    Start streaming repo

---
+++ lib/streaming_data/streaming_repo.ex
@@ -0,0 +1,20 @@
+defmodule StreamingData.StreamingRepo do
+  @repo StreamingData.Repo
+  @endpoint StreamingData.Endpoint
+
+  def insert!(model, options \\ []) do
+    if from = Dict.get(options, :from) do
+      Dict.delete(options, :from)
+    end
+    model = @repo.insert!(model, options)
+
+    payload = StreamingData.ContactSerializer.format(model)
+    if from do
+      %{channel_pid: channel_pid} = from
+      @endpoint.broadcast_from channel_pid, "contacts:index", "new", payload
+    else
+      @endpoint.broadcast "contacts:index", "new", payload
+    end
+    model
+  end
+end
--- web/channels/contacts_channel.ex
+++ web/channels/contacts_channel.ex
@@ -3,14 +3,37 @@ defmodule StreamingData.ContactsChannel do
   alias StreamingData.Contact
   alias StreamingData.Repo
 
-  def join("contacts:index", auth_msg, socket) do
+  def join("contacts:" <> _scoped_id, auth_msg, socket) do
+    IO.inspect _scoped_id
     {:ok, socket}
   end
 
   def handle_in("all", payload, socket) do
+    IO.inspect payload
     contacts = Repo.all(Contact)
 
     payload = StreamingData.ContactSerializer.format(contacts)
+    # payload = payload
+    # |> Map.put(:meta, %{since: 3})
+
+    {:reply, {:ok, payload}, socket}
+  end
+
+  def handle_in("find", id, socket) do
+    contact = Repo.get(Contact, id)
+
+    payload = StreamingData.ContactSerializer.format(contact)
+
+    {:reply, {:ok, payload}, socket}
+  end
+
+  def handle_in("update", %{"data" => %{"id" => id, "attributes" => attributes, "type" => "contacts"}}, socket) do
+    contact = Repo.get(Contact, id)
+    |> Contact.changeset(normalize_attributes(attributes))
+    |> Repo.update!
+
+    payload = StreamingData.ContactSerializer.format(contact)
+    broadcast_from! socket, "update", payload
 
     {:reply, {:ok, payload}, socket}
   end
@@ -18,10 +41,9 @@ defmodule StreamingData.ContactsChannel do
   def handle_in("create", %{"data" => %{"attributes" => attributes, "type" => "contacts"}}, socket) do
     contact = %Contact{}
     |> Contact.changeset(normalize_attributes(attributes))
-    |> Repo.insert!
+    |> StreamingData.StreamingRepo.insert!([from: socket])
 
     payload = StreamingData.ContactSerializer.format(contact)
-    broadcast! socket, "new_contact", payload
 
     {:reply, {:ok, payload}, socket}
   end
@@ -32,3 +54,4 @@ defmodule StreamingData.ContactsChannel do
     |> Enum.into(%{})
   end
 end
+
