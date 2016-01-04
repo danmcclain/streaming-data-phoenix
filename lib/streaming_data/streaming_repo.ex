35f881205594b365d6884e44742ab9bbd34495f8
Dan McClain <git@danmcclain.net>
2016-01-04 15:02:48 -0500

    Refactor ! methods

--- lib/streaming_data/streaming_repo.ex
+++ lib/streaming_data/streaming_repo.ex
@@ -3,34 +3,30 @@ defmodule StreamingData.StreamingRepo do
   @endpoint StreamingData.Endpoint
 
   def insert!(model, options \\ []) do
-    if from = Dict.get(options, :from) do
-      Dict.delete(options, :from)
-    end
-    model = @repo.insert!(model, options)
-
-    payload = StreamingData.ContactSerializer.format(model)
-    if from do
-      %{channel_pid: channel_pid} = from
-      @endpoint.broadcast_from channel_pid, "contacts:index", "new", payload
-    else
-      @endpoint.broadcast "contacts:index", "new", payload
-    end
-    model
+    brodcast_action!(&@repo.insert!/2, model, options, "new")
   end
 
   def update!(model, options \\ []) do
+    brodcast_action!(&@repo.update!/2, model, options, "update")
+  end
+
+  defp brodcast_action!(action_func, model, options, event) do
     if from = Dict.get(options, :from) do
       Dict.delete(options, :from)
     end
-    model = @repo.update!(model, options)
+    model = action_func.(model, options)
+
+    channel = "contacts:index"
 
     payload = StreamingData.ContactSerializer.format(model)
+
     if from do
       %{channel_pid: channel_pid} = from
-      @endpoint.broadcast_from channel_pid, "contacts:index", "new", payload
+      @endpoint.broadcast_from channel_pid, channel, event, payload
     else
-      @endpoint.broadcast "contacts:index", "update", payload
+      @endpoint.broadcast channel, event, payload
     end
+
     model
   end
 end
